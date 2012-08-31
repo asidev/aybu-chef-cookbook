1ae80f75c8d0ddff314f0a4da1c157bcb741f64e
Giacomo Bagnoli <g.bagnoli@asidev.com>
2012-08-31 15:23:07 +0200

    Added varnish vcl to remove all non-aybu cookies from requests

---
+++ recipes/varnish.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: aybu
+# Recipe:: varnish
+#
+# Copyright 2012, Asidev s.r.l.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+additional_includes = node['varnish']['additional_includes']
+additional_includes << "aybu.vcl"
+node.set['varnish']['additional_includes'] = additional_includes
+
+template "#{node["varnish"]["dir"]}/aybu.vcl" do
+  source "aybu.vcl.erb"
+  mode 0644
+  owner "root"
+  group "root"
+  notifies :run, "execute[reload_vcl]"
+end
